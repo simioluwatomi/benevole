f36dd57db1c2377af4d9e5dc47f2229bc9b17993
simioluwatomi <deifilius1@gmail.com>
2020-02-11 02:14:13 +0100

    Separate auth routes from web.php in order to use tuple notation in route files

---
+++ routes/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Auth Routes
+|--------------------------------------------------------------------------
+|
+| Here is where authentication routes of the application are defined. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes(['verify' => true]);
