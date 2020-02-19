2ce97d29195f3f700a88dfadd345c6f73f8d9808
simioluwatomi <deifilius1@gmail.com>
2020-02-19 13:01:14 +0100

    Create new vue components

---
+++ resources/js/components/InputComponent.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <b-form-input
+        :name="inputName"
+        :id="inputId"
+        v-model="value"
+        :state="inputState"
+        :placeholder="placeholder"
+        :required="!!required"
+    ></b-form-input>
+
+</template>
+
+<script>
+    export default {
+        name: "InputComponent",
+        props: {
+            inputName: {
+                type: String,
+                required: true,
+            },
+            inputId: {
+                type: String,
+                required: true,
+            },
+            inputState: {
+                required: true,
+            },
+            oldInput: {
+                type: String
+            },
+            placeholder: {
+                type: String,
+            },
+            required: {
+                type: Boolean,
+                default: false,
+            }
+        },
+        data() {
+            return {
+                value: this.oldInput ? this.oldInput : null,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
