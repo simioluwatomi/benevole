027213c4a946d09cc6a42e614e612a1b2e8b433d
Simi Oluwatomi <deifilius1@gmail.com>
2020-02-19 12:59:40 +0100

    Update StoreVolunteerOpportunityRequest rules and cast some inputs to type integer

--- app/Http/Requests/StoreVolunteerOpportunityRequest.php
+++ app/Http/Requests/StoreVolunteerOpportunityRequest.php
@@ -26,12 +26,13 @@ class StoreVolunteerOpportunityRequest extends FormRequest
     {
         return [
             'category_id'        => ['required'],
-            'title'              => ['required'],
-            'description'        => ['required'],
+            'title'              => ['required', 'min:8'],
+            'description'        => ['required', 'min:15'],
             'requirements'       => ['required', 'array'],
-            'min_hours_per_week' => ['required'],
-            'max_hours_per_week' => ['required'],
-            'duration'           => ['required'],
+            'requirements.*'     => ['required', 'string', 'distinct', 'min:5'],
+            'min_hours_per_week' => ['required', 'between:1,8', 'lt:max_hours_per_week'],
+            'max_hours_per_week' => ['required', 'between:1,10', 'gt:min_hours_per_week'],
+            'duration'           => ['required', 'max:12'],
         ];
     }
 
@@ -43,7 +44,25 @@ class StoreVolunteerOpportunityRequest extends FormRequest
     public function messages()
     {
         return [
-            'category_id.required' => 'A category is required',
+            'category_id.required'    => 'The category is required.',
+            'requirements.*.required' => 'The requirements field is required.',
+            'requirements.*.string'   => 'The requirements field must be a string.',
+            'requirements.*.distinct' => 'The requirements field has a duplicate value.',
+            'requirements.*.min'      => 'The requirements field must be at least :min characters.',
+            'min_hours_per_week.lt'   => 'The :attribute must be less than the max hours per week',
+            'max_hours_per_week.gt'   => 'The :attribute must be greater than the min hours per week',
         ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'duration'           => (int) $this->input('duration'),
+            'min_hours_per_week' => (int) $this->input('min_hours_per_week'),
+            'max_hours_per_week' => (int) $this->input('max_hours_per_week'),
+        ]);
     }
 }
