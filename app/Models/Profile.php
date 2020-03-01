ba2dcad5b5ac050a24435b0260db1bdb7f108395
simioluwatomi <deifilius1@gmail.com>
2020-03-01 16:33:39 +0100

    Wrote model doc blocks for Profile.php

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Profile.
+ *
+ * @property int                             $id
+ * @property int                             $user_id
+ * @property null|string                     $organization_name
+ * @property null|\Illuminate\Support\Carbon $created_at
+ * @property null|\Illuminate\Support\Carbon $updated_at
+ * @property \App\Models\User                $user
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile whereOrganizationName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Profile whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Profile extends Model
 {
     /**
